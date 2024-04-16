75d6ca60c5e6f9dce6c5ab03521853ab95ce98c4
Morgan Cowans <cowansm@sheridancollege.ca>
2024-04-16 18:28:06 -0400

    work for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {FindCourseComponent} from "./components/find-course/find-course.component";
+import {UpdateCourseComponent} from "./components/update-course/update-course.component";
+import {AddCourseComponent} from "./components/add-course/add-course.component";
+import {DeleteCourseComponent} from "./components/delete-course/delete-course.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'courses', component: FindCourseComponent},
+  {path: 'courses/:id/edit', component:UpdateCourseComponent},
+  {path: 'addcourse',component:AddCourseComponent},
+  {path: 'deletecourse/:id',component:DeleteCourseComponent}
+];
